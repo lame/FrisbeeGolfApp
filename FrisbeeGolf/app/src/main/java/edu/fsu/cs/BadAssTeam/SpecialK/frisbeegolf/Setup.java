41588fd74b43ed746f521fac2abc9ec182bb050e
cyber ninja <cyberninja@localhost>
2014-07-19 17:55:58 -0400

    fixed imports

--- FrisbeeGolf/app/src/main/java/edu/fsu/cs/BadAssTeam/SpecialK/frisbeegolf/Setup.java
+++ FrisbeeGolf/app/src/main/java/edu/fsu/cs/BadAssTeam/SpecialK/frisbeegolf/Setup.java
@@ -1,5 +1,9 @@
 package edu.fsu.cs.BadAssTeam.SpecialK.frisbeegolf;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
 /**
  * Created by lance on 7/19/14.
  */
@@ -8,7 +12,7 @@ public class Setup extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.Setup);
+        setContentView(R.layout.config);
     }
 
 }
