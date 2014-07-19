3a9a0a87b05a88f1319e328051a9552c44637f31
cyber ninja <cyberninja@localhost>
2014-07-19 17:43:08 -0400

    new intent

---
+++ FrisbeeGolf/app/src/main/java/edu/fsu/cs/BadAssTeam/SpecialK/frisbeegolf/Setup.java
@@ -0,0 +1,14 @@
+package edu.fsu.cs.BadAssTeam.SpecialK.frisbeegolf;
+
+/**
+ * Created by lance on 7/19/14.
+ */
+public class Setup extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.Setup);
+    }
+
+}
