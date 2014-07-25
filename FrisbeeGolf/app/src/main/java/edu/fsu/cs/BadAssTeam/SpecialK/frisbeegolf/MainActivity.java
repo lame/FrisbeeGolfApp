647b5d7b051e3e19a2bf4bdd26c9e5c4e7643c1e
k <k@localhost>
2014-07-25 02:26:33 -0400

    fragment fix

--- FrisbeeGolf/app/src/main/java/edu/fsu/cs/BadAssTeam/SpecialK/frisbeegolf/MainActivity.java
+++ FrisbeeGolf/app/src/main/java/edu/fsu/cs/BadAssTeam/SpecialK/frisbeegolf/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,8 +16,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+
+//import android.support.v4.widget.DrawerLayout;
 
 public class MainActivity extends FragmentActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
