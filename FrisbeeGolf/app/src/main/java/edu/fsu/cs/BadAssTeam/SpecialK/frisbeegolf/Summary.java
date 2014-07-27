19985f2d6b61a7eb790c731c18220600c410a227
cyber ninja <cyberninja@localhost>
2014-07-27 19:26:14 -0400

    added Mclean course

--- FrisbeeGolf/app/src/main/java/edu/fsu/cs/BadAssTeam/SpecialK/frisbeegolf/Summary.java
+++ FrisbeeGolf/app/src/main/java/edu/fsu/cs/BadAssTeam/SpecialK/frisbeegolf/Summary.java
@@ -1,7 +1,18 @@
 package edu.fsu.cs.BadAssTeam.SpecialK.frisbeegolf;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
 /**
  * Created by lance on 7/27/14.
  */
-public class Summary {
+public class Summary extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.summary);
+    }
 }
